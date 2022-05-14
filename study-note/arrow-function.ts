9d3469717577a760bb4a2ecaf7d437229ead5176
hyej <hyejeyh@gmail.com>
2022-05-14 21:13:31 +0900

    arrow function with ts

---
+++ study-note/arrow-function.ts
@@ -0,0 +1,7 @@
+/**
+ * 화살표 함수
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+ */
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const sum = (a: number, b: number): number => a + b;
