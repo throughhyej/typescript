e08f198a72512793a115da9c33e41f318a28d43a
hyej <hyejeyh@gmail.com>
2022-05-15 11:36:08 +0900

    vue에서 동작하도록 조치

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import App from "./App.vue";
 
 Vue.config.productionTip = false;
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { startApp } from "./app.ts";
+startApp();
+
 new Vue({
   render: (h) => h(App),
 }).$mount("#app");
